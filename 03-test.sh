11b172a274c61c90775918f410fc4b0eda23da32
nithinsagar24 <nithinsagar42@gmail.com>
2025-10-06 21:10:27 +0530

    conversation

--- 03-test.sh
+++ 03-test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-name1 = "nithin"
-name2 = "junnu"
+name1="nithin"
+name2="junnu"
 
-echo "$name1:how are you $name2"
-echo "$name2:i am fine what about you $name1"
-echo "$name1:what about sister and alludu"
+echo "$name1: : how are you $name2"
+echo "$name2: : i am fine what about you $name1"
+echo "$name1: : what about sister and alludu"
